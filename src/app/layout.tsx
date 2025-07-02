891fe69373222034fabdc77cbb6cdf635b2b5689
Ernesto Mandlate <ernesto.mandlate@undp.org>
2025-07-02 14:46:16 +0200

    fixing problems of layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="" wotdisconnected="true">
+      <body className="">
         {children}
       </body>
     </html>
