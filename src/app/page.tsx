91f8147881ec7b07255197bf2d1dec1d5ca63d5f
Ernesto Mandlate <ernesto.mandlate@undp.org>
2025-07-17 12:12:11 +0200

    mas 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
 "use client";
+import Contact from "@/componets/Contact";
 import styles from "../app/style";
 
-import {
-  Navbar,
-  Hero,
-  Abaut,
-  Skills,
-  Projects,
-  //Contact,
-  Footer,
-} from "../componets";
+import { Navbar, Hero, Abaut, Skills, Footer } from "../componets";
 
 import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
@@ -41,7 +34,7 @@ const page = () => (
 
       <div className={`bg-black ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          <Hero /> <Abaut /> <Skills /> <Projects />
+          <Hero /> <Abaut /> <Skills /> <Contact />
         </div>
       </div>
 
--- src/componets/index.ts
+++ src/componets/index.ts
@@ -1,10 +1,10 @@
 import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Abaut from "./Abaut";
-import Projects from "./Projects ";
-import ProjectCards from "./ProjectCards";
+//import Projects from "./Projects ";
+//import ProjectCards from "./ProjectCards";
 import Skills from "./Skills";
-//import Contact from "./Contact";
+import Contact from "./Contact";
 import Button from "./Button";
 import Footer from "./Footer";
 
@@ -12,10 +12,10 @@ export {
   Navbar,
   Hero,
   Abaut,
-  Projects,
-  ProjectCards,
+  //Projects,
+  //ProjectCards,
   Skills,
- // Contact,
+  Contact,
   Button,
   Footer,
 };
